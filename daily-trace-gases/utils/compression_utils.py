masking fix

daily-trace-gases/utils/compression_utils.py
@@ -21,7 +21,8 @@ def compress_mf_tif(input_path, output_path):
         meta = src.meta.copy()
 
         # mask no data to 0 (important, before the min/max)
-        float_data = np.where(float_data == meta["nodata"], 0.0, float_data)
+        valid = np.where(float_data == meta["nodata"], 0, 1)
+        float_data = np.where(valid == 0, 0.0, float_data)
 
         # rescale to 0-255
         min_val, max_val = np.nanmin(float_data), np.nanmax(float_data)
@@ -30,8 +31,8 @@ def compress_mf_tif(input_path, output_path):
         rescaled = 255 * (float_data - min_val) / (max_val - min_val)
         uint8_data = np.nan_to_num(rescaled).astype(np.uint8)
 
-        # then still set those to 0 (otherwise they were
-        uint8_data = np.where(float_data == meta["nodata"], 0, uint8_data).astype(np.uint8)
+        # then still set those to 0
+        uint8_data = np.where(valid == 0, 0, uint8_data).astype(np.uint8)
 
         # set compression
         meta.update(